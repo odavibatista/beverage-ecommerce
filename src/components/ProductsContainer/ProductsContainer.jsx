cc88694a8e1f92b81c9642cd71c85350eb9af19d
Davi Batista <davideosmar11@gmail.com>
2023-05-01 11:01:04 -0300

    V2.1 | feat: new products added, new categories and .jsx components

--- src/components/ProductsContainer/ProductsContainer.jsx
+++ src/components/ProductsContainer/ProductsContainer.jsx
@@ -4,17 +4,17 @@ import { ProductCard } from "../ProductCard/ProductCard.jsx";
 
 
 export function ProductsContainer   ()  {
-    let productOne = Math.floor(Math.random()*27 + 1)
-    let productTwo = Math.floor(Math.random()*27 + 1)
-    if(productTwo === productOne)   productTwo = Math.floor(Math.random()*27 + 1)
-    let productThree = Math.floor(Math.random()*27 + 1)
-    if(productThree === productOne || productThree === productTwo)   productThree = Math.floor(Math.random()*27 + 1)
-    let productFour = Math.floor(Math.random()*27 + 1)
-    if(productFour === productOne || productFour === productTwo || productFour === productThree)   productFour = Math.floor(Math.random()*27 + 1)
-    let productFive = Math.floor(Math.random()*27 + 1)
-    if(productFive === productOne || productFive === productTwo || productFive === productThree || productFive === productFour)   productFive = Math.floor(Math.random()*27 + 1)
-    let productSix = Math.floor(Math.random()*27 + 1)
-    if(productSix === productOne || productSix === productTwo || productSix === productThree || productSix === productFour || productSix === productFive)   productSix = Math.floor(Math.random()*27 + 1)
+    let productOne = Math.floor(Math.random()* 35 + 1)
+    let productTwo = Math.floor(Math.random()* 35 + 1)
+    if(productTwo === productOne)   productTwo = Math.floor(Math.random()* 35 + 1)
+    let productThree = Math.floor(Math.random()* 35 + 1)
+    if(productThree === productOne || productThree === productTwo)   productThree = Math.floor(Math.random()* 35 + 1)
+    let productFour = Math.floor(Math.random()* 35 + 1)
+    if(productFour === productOne || productFour === productTwo || productFour === productThree)   productFour = Math.floor(Math.random()* 35 + 1)
+    let productFive = Math.floor(Math.random()* 35 + 1)
+    if(productFive === productOne || productFive === productTwo || productFive === productThree || productFive === productFour)   productFive = Math.floor(Math.random()* 35 + 1)
+    let productSix = Math.floor(Math.random()* 35 + 1)
+    if(productSix === productOne || productSix === productTwo || productSix === productThree || productSix === productFour || productSix === productFive)   productSix = Math.floor(Math.random()* 35 + 1)
 
     return (
     <main id="products-container">
